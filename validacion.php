3aad70f713a3a38720450d1a7ef5a7664b8046e9
aristides1000 <aristides1000@gmail.com>
2025-04-09 14:32:32 +0000

    Feat: finish tema 11

--- validacion.php
+++ validacion.php
@@ -145,5 +145,23 @@ if(isset($_POST['val'])) {
         <?php
       }
       break;
+    case 5 : //eliminacion de productos en el carrito
+      $sql = "delete from carrito where id_usuario='$_SESSION[id_usuario]' and id_producto='$_POST[idp]'";
+      mysqli_query($link,$sql);
+      if (mysqli_error($link))
+      {?>
+        <script> alert("Error en la eliminación del producto. "); </script>
+        <meta http-equiv="refresh" content="2;URL=./carrito.php" />
+        <?php
+      }
+      else
+      {
+        $sql1 = "update producto set cantidad = cantidad +'$_POST[cant]' where id_producto='$_POST[idp]' ";
+        mysqli_query($link,$sql1);?>
+        <script> alert("El producto ha sido eliminado exitosamente "); </script>
+        <meta http-equiv="refresh" content="2;URL=./carrito.php" />
+        <?php
+      }
+      break;
   }
 }
